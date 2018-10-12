1bf5445372718faad5378dee874dfd14ffec221e
Jessica Temporal <jessicatemporal@gmail.com>
2018-10-12 16:55:59 -0300

    Serialize_when_none_false

--- pessoa/models.py
+++ pessoa/models.py
@@ -4,5 +4,5 @@ from schematics.types import StringType, ListType, ModelType, IntType
 
 class Pessoa(Model):
     nome = StringType(required=True)
-    idade = IntType()
-    linguagens = ListType(StringType)
+    idade = IntType(serialize_when_none=False)
+    linguagens = ListType(StringType, serialize_when_none=False)
