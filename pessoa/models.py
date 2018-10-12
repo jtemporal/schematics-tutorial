2beeea697b92d1ebcc6ba29006dd915c5f6798a5
Jessica Temporal <jessicatemporal@gmail.com>
2018-10-12 17:05:59 -0300

    Serialized_name

--- pessoa/models.py
+++ pessoa/models.py
@@ -4,8 +4,9 @@ from schematics.types import StringType, ListType, ModelType, IntType
 
 class Pessoa(Model):
     nome = StringType(required=True, serialize_when_none=False,
-                      deserialize_from="name")
-    idade = IntType(serialize_when_none=False, deserialize_from="age")
+                      deserialize_from="name", serialized_name="name")
+    idade = IntType(serialize_when_none=False, deserialize_from="age",
+                    serialized_name="age")
     linguagens = ListType(StringType, serialize_when_none=False,
-                          deserialize_from="lang")
+                          deserialize_from="lang", serialized_name="blabla")
 
