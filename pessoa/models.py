923923a25c9bd5f7caca99981d7f62aca2bc9d8c
Jessica Temporal <jessicatemporal@gmail.com>
2018-10-13 11:15:27 -0300

    Pet

--- pessoa/models.py
+++ pessoa/models.py
@@ -4,6 +4,13 @@ from schematics.types import StringType, ListType, ModelType, IntType
 from udtypes import SizedNumberType
 
 
+class Cachorrinho(Model):
+    nome = StringType(required=True, deserialize_from="name",
+                      serialized_name="name")
+    cor = StringType(required=True, deserialize_from="fur",
+                      serialized_name="fur")
+
+
 class Pessoa(Model):
     nome = StringType(required=True, deserialize_from="name",
                       serialized_name="name")
@@ -11,3 +18,5 @@ class Pessoa(Model):
                             serialized_name="age")
     linguagens = ListType(StringType, serialize_when_none=False,
                           deserialize_from="lang", serialized_name="blabla")
+    cachorrinhos = ListType(ModelType(Cachorrinho), serialize_when_none=False,
+                            deserialize_from="pets", serialized_name="pets")
